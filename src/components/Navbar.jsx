f8313cd065268c9c9f5662f6ec056a8e163ac99c
NKC <nkcpathuri@gmailcom>
2025-03-13 22:24:13 +0530

    My Portfolio Code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,180 @@
+import React, { useState, useEffect } from "react";
+import { Link as LinkR } from "react-router-dom";
+import styled, { useTheme } from "styled-components";
+import { MenuRounded } from "@mui/icons-material";
+import nkcImg from '../images/nkc.png';
+import inF from '../images/infinitewizcrafts.png';
+
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+`;
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+
+const NavLogo = styled(LinkR)`
+  width: 85px;
+  height: 85px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+`;
+
+const LogoImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  position: absolute;
+  opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+  transition: opacity 0.8s ease-in-out, transform 0.8s ease-in-out;
+  transform: ${({ isActive }) => (isActive ? "scale(1.1)" : "scale(1)")};
+`;
+
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavLink = styled.a`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+
+const MobileIcon = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+const MobileMenu = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  position: absolute;
+  top: 80px;
+  right: 0;
+
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) =>
+    isOpen ? "translateY(0)" : "translateY(-100%)"};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+`;
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeImage, setActiveImage] = useState(0);
+  const theme = useTheme();
+  const images = [nkcImg, inF];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveImage((prev) => (prev === 0 ? 1 : 0));
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">
+          {images.map((img, index) => (
+            <LogoImage
+              key={index}
+              src={img}
+              alt={images}
+              isActive={index === activeImage}
+            />
+          ))}
+        </NavLogo>
+
+        <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+          <MenuRounded style={{ color: "inherit" }} />
+        </MobileIcon>
+
+        <NavItems>
+          <NavLink href="#About">About</NavLink>
+          <NavLink href="#Skills">Skills</NavLink>
+          <NavLink href="#Certifications">Certifications</NavLink>
+          <NavLink href="#Experience">Experience</NavLink>
+          <NavLink href="#Education">Education</NavLink>
+          <NavLink href="#Projects">Projects</NavLink>
+        </NavItems>
+
+        {isOpen && (
+          <MobileMenu isOpen={isOpen}>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#About">
+              About
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Skills">
+              Skills
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Certifications">
+              Certifications
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Experience">
+              Experience
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Projects">
+              Projects
+            </NavLink>
+            <NavLink onClick={() => setIsOpen(!isOpen)} href="#Education">
+              Education
+            </NavLink>      
+          </MobileMenu>
+        )}
+        
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
